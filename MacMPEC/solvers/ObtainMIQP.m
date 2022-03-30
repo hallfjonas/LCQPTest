dcaaea728c3353027fab2e72b5467ea21193024e
hallfjonas <hall.f.jonas@gmail.com>
2022-03-30 11:26:08 -0400

    include MIQP reformulation and solve via gurobi

---
+++ MacMPEC/solvers/ObtainMIQP.m
@@ -0,0 +1,103 @@
+
+function [problem] = ObtainMIQP(lcqp_formulation)
+
+%% Extract variables, initial guess and box constraints
+Q = lcqp_formulation.Q;
+nV = size(Q,1);
+problem.nV = nV;
+
+lb = [];
+ub = [];
+if (isfield(lcqp_formulation, "lb"))
+    lb = lcqp_formulation.lb;
+end
+
+if (isfield(lcqp_formulation, "ub"))
+    ub = lcqp_formulation.ub;
+end
+
+g = lcqp_formulation.g;
+
+nC = 0;
+if (isfield(lcqp_formulation, "A"))
+    A = lcqp_formulation.A;
+    lbA = lcqp_formulation.lbA;
+    ubA = lcqp_formulation.ubA;
+    nC = size(A,1);
+end
+problem.nC = nC;
+
+%% Complementarities
+L = lcqp_formulation.L;
+R = lcqp_formulation.R;
+nComp = size(L,1);
+problem.nComp = nComp;
+
+M = 10e3;
+lb_L = zeros(nComp,1);
+ub_L = M*ones(nComp,1);
+lb_R = zeros(nComp,1);
+ub_R = M*ones(nComp,1);
+
+if (isfield(lcqp_formulation, "lb_L"))
+    lb_L = lcqp_formulation.lb_L;
+end
+if (isfield(lcqp_formulation, "ub_L"))
+    ub_L = lcqp_formulation.ub_L;
+end
+
+if (isfield(lcqp_formulation, "lb_R"))
+    lb_R = lcqp_formulation.lb_R;
+end
+if (isfield(lcqp_formulation, "ub_R"))
+    ub_R = lcqp_formulation.ub_R;
+end
+
+%% Now introduce binary variables and extend to that diemnsion
+% obj
+Q_new = sparse([Q, zeros(nV,2*nComp); zeros(2*nComp,nV), zeros(2*nComp,2*nComp)]);
+g_new = [g; zeros(2*nComp,1)];
+problem.Q = Q_new;
+problem.g = g_new;
+
+% constraints
+A_new = [A, zeros(nC,2*nComp)];
+L_new = [L, zeros(nComp,2*nComp)];
+R_new = [R, zeros(nComp,2*nComp)];
+
+% Lx - ub_L*z <= 0
+A_comp_L = [L, - ub_L.*eye(nComp), zeros(nComp,nComp)];
+A_comp_R = [R, zeros(nComp,nComp), - ub_R.*eye(nComp)];
+A_bin = [zeros(nComp, nV), eye(nComp), eye(nComp)];
+
+problem.L = L_new;
+problem.R = R_new;
+problem.lb_L = lb_L;
+problem.lb_R = lb_R;
+A = [A_new; -A_new; L_new; -L_new; R_new; -R_new; A_comp_L; A_comp_R; A_bin];
+rhs = [ubA; -lbA; ub_L; -lb_L; ub_R; -lb_R; zeros(nComp,1); zeros(nComp,1); ones(nComp,1)];
+
+% Have box constraints?
+if length(lb) > 0 
+    E = eye(nV);
+    for i=1:length(lb)
+        if (lb(i) >  -inf)
+            A = [A; -E(i,:), zeros(1, 2*nComp)];
+            rhs = [rhs; -lb(i)];
+        end
+    end
+end
+if length(ub) > 0
+    E = eye(nV);
+    for i=1:length(ub)
+        if (ub(i) <inf)
+            A = [A; E(i,:), zeros(1, 2*nComp)];
+            rhs = [rhs; ub(i)];
+        end
+    end
+end
+
+problem.A = sparse(A);
+problem.rhs = rhs;
+
+return
---
+++ MacMPEC/solvers/SolveMIQP.m
@@ -0,0 +1,110 @@
+function [solutions] = SolveMIQP(name)
+
+import casadi.*;
+
+% Remember the name?
+problemname = name;
+
+%% Create the LCQP
+cd MacMPECMatlab/WO_ampl;
+run([name, '.m']);
+cd ../..;
+
+if (~isfield(problem, 'A'))
+    problem.A = [];
+    problem.lbA = [];
+    problem.ubA = [];
+end
+
+if (~exist('lb'))
+    lb = [];
+end
+
+if (~exist('ub'))
+    ub = [];
+end
+
+problem.lb = lb;
+problem.ub = ub;
+
+%% Change to MIQP setting
+MIQP_formulation = ObtainMIQP(problem);
+
+%% Build solver
+varnames = {};
+nV = MIQP_formulation.nV;
+nC = MIQP_formulation.nC;
+nComp = MIQP_formulation.nComp;
+for i=1:nV
+    varnames{i} = ['x', num2str(i)];
+end
+for i=1:nComp
+    varnames{nV+i} = ['zL', num2str(i)];
+end
+for i=1:nComp
+    varnames{nV+nComp+i} = ['zR', num2str(i)];
+end
+model.varnames = varnames;
+
+% TODO: Where should I handle sparsity? sparse( )
+model.Q = MIQP_formulation.Q;
+model.obj = MIQP_formulation.g;
+
+model.A = MIQP_formulation.A;
+model.rhs = MIQP_formulation.rhs;
+model.sense = '<';
+
+gurobi_write(model, 'qp.lp');
+
+% Set variable types
+for i=1:nV
+    model.vtype(i) = 'C';
+end
+for i=1:nComp
+    model.vtype(nV+i) = 'B';
+    model.vtype(nV+nComp+i) = 'B';
+end
+
+stats = {};
+
+if strcmp(problemname, 'jr1')
+    disp("CHECK THIS");
+end
+
+%% Run the solver
+% TODO: Can I get the solver time from results struct?
+params.outputflag = 1; 
+tic
+results = gurobi(model, params);
+solutions.stats.elapsed_time = toc;
+% Evaluate objective
+
+import casadi.*
+if (exist('w'))
+    Obj = Function('Obj', {w}, {obj});
+else
+    Q = MIQP_formulation.Q(1:nV,1:nV);
+    g = MIQP_formulation.g(1:nV,1:nV);
+    Obj = @(x) 0.5*x'*Q*x + g'*x;
+end
+
+% Save the solution and stats
+% solutions.stats.elapsed_time = results.runtime;
+solutions.stats.exit_flag = 1- strcmp(results.status, 'OPTIMAL');
+
+solutions.x = zeros(nV+2*nComp,1);
+solutions.stats.compl = inf;
+solutions.stats.obj = inf;
+
+% Evaluate complementarity
+if solutions.stats.exit_flag == 0
+    compl_L = MIQP_formulation.L*results.x - MIQP_formulation.lb_L;
+    compl_R = MIQP_formulation.R*results.x - MIQP_formulation.lb_R;
+    compl = compl_L'*compl_R;
+    solutions.x = results.x;
+    solutions.stats.compl = compl;
+    solutions.stats.obj = full(Obj(solutions.x(1:nV)));
+    results.x(1:nV)
+end
+
+end
