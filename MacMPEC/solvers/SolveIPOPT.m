66472f013dabd7fb7d8b40f5044818d2094f591c
hallfjonas <hall.f.jonas@gmail.com>
2022-04-20 11:40:40 -0400

    use solver timing without overhead

--- MacMPEC/solvers/SolveIPOPT.m
+++ MacMPEC/solvers/SolveIPOPT.m
@@ -11,7 +11,7 @@ opts_ipopt = struct;
 opts_ipopt.ipopt.nlp_scaling_method = 'none';
 opts_ipopt.ipopt.print_level = 0;
 opts_ipopt.ipopt.bound_relax_factor = eps;
-opts_ipopt.print_time = 0;
+opts_ipopt.print_time = 1;
 opts_ipopt.print_out = 0;
 opts_ipopt.ipopt.mu_strategy = 'adaptive';
 opts_ipopt.ipopt.mu_oracle = 'quality-function';
@@ -30,10 +30,9 @@ solver = nlpsol('solver', 'ipopt', nlp, opts_ipopt);
 stats.exit_flag = 1;
 stats.elapsed_time = 0;
 
-tic;
 sol = CallIPOPTSolver( solver, IPOPT_formulation, 0 );
 w_opt = sol.x;
-stats.elapsed_time = stats.elapsed_time + toc;
+stats.elapsed_time = solver.stats.t_proc_total;
 
 rho = IPOPT_formulation.rho0;
 stats.iters_outer = 0;
@@ -48,11 +47,10 @@ while(true)
         break;
     end
         
-    tic;
     sol = CallIPOPTSolver( solver, IPOPT_formulation, rho );
     w_opt = sol.x;
     
-    stats.elapsed_time = stats.elapsed_time + toc;
+    stats.elapsed_time = stats.elapsed_time + solver.stats.t_proc_total;
     stats.iters_outer = stats.iters_outer + 1;
     
     rho = rho*IPOPT_formulation.beta;
