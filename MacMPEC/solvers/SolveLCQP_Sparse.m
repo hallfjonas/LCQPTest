ef09f93f570ff59dc5c26628a40582c1b7d8e8e8
hallfjonas <hall.f.jonas@gmail.com>
2022-04-22 11:18:48 -0400

    remove deprecated checking for box constraints

--- MacMPEC/solvers/SolveLCQP_Sparse.m
+++ MacMPEC/solvers/SolveLCQP_Sparse.m
@@ -4,7 +4,7 @@ import casadi.*;
 
 addpath("~/LCQPow/build/lib");
 currdir = pwd;
-cd MacMPECMatlab;
+cd MacMPECMatlab/;
 run([name, '.m']);
 cd(currdir);
 
@@ -14,34 +14,19 @@ if (~isfield(problem, 'A'))
     problem.ubA = [];
 end
 
-if (~exist('lb'))
-    lb = [];
-end
-
-if (~exist('ub'))
-    ub = [];
-end
-
-problem.lb = lb;
-problem.ub = ub;
-
-% Convert to sparse matrices
-problem.Q = sparse(problem.Q);
-problem.A = sparse(problem.A);
-problem.L = sparse(problem.L);
-problem.R = sparse(problem.R);
-
 % Solve LCQP
-params.printLevel = 0;
+params.printLevel = 2;
 params.qpSolver = 1;
+
+% params.stationarityTolerance = 10e-6;
 tic;
 [solution.x,solution.y,solution.stats] = LCQPow(...
-    problem.Q, ...
+    sparse(problem.Q), ...
     problem.g, ...
-    problem.L, ...
-    problem.R, ...
+    sparse(problem.L), ...
+    sparse(problem.R), ...
     [], [], [], [], ...
-    problem.A, ...
+    sparse(problem.A), ...
     problem.lbA, ...
     problem.ubA, ...
     problem.lb, ...
@@ -49,11 +34,12 @@ tic;
     params ...
 );
 solution.stats.elapsed_time_w_overhead = toc;
+
+solution.stats.obj = full(problem.Obj(solution.x));
+solution.stats.compl = full(problem.Phi(solution.x));
 solution.stats.n_x = size(problem.Q, 1);
 solution.stats.n_c = size(problem.A, 1);
 solution.stats.n_comp = size(problem.L, 1);
-solution.stats.obj = full(problem.Obj(solution.x));
-
 
 end
 
