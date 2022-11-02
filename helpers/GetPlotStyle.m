034a9bf26b25e627ad02d622e05dd2a7380815a3
hallfjonas <hall.f.jonas@gmail.com>
2022-11-02 10:58:56 -0400

    improve colors

--- helpers/GetPlotStyle.m
+++ helpers/GetPlotStyle.m
@@ -1,6 +1,6 @@
 function [ res ] = GetPlotStyle(solname)
 
-    colors = distinguishable_colors(20);
+    colors = linspecer(7);
     markers = {'o', '+', '*', 'x', 's', 'd', '^', 'v', '>', '<', 'p', 'h'};
     linestyles = {'-','--',':','-.'};
 
@@ -13,49 +13,49 @@ function [ res ] = GetPlotStyle(solname)
     res = {};
 
     if solname == "SolveLCQPow0"
-        res.colidx = 1;
+        res.colidx = 2;
         res.markidx = 1;
         res.lsidx = 1;
         res.label = "LCQPow qpOASES";
     elseif solname == "SolveLCQPow0wOverhead"
-        res.colidx = 4;
+        res.colidx = 2;
         res.markidx = 3;
         res.lsidx = 1;
-        res.label = "LCQPow qoOASES overhead";
+        res.label = "LCQPow qpOASES overhead";
     elseif solname == "SolveLCQPow1"
-        res.colidx = 1;
+        res.colidx = 2;
         res.markidx = 3;
         res.lsidx = 1;
-        res.label = "LCQPow qoOASES MA57";
+        res.label = "LCQPow qpOASES MA57";
     elseif solname == "SolveLCQPow2"
-        res.colidx = 8;
+        res.colidx = 4;
         res.markidx = 2;
         res.lsidx = 4;
         res.label = "LCQPow OSQP";
     elseif solname == "SolveMIQP"
-        res.colidx = 2;
+        res.colidx = 1;
         res.markidx = 4;
         res.lsidx = 2;
         res.label = "Gurobi";
     elseif solname == "SolveIPOPTPen"
         res.colidx = 3;
         res.markidx = 5;
-        res.lsidx = 3;
+        res.lsidx = 1;
         res.label = "IPOPT penalty";
     elseif solname == "SolveIPOPTNLP"
-        res.colidx = 20;
+        res.colidx = 5;
         res.markidx = 6;
-        res.lsidx = 3;
+        res.lsidx = 2;
         res.label = "IPOPT NLP";
     elseif solname == "SolveIPOPTReg"
-        res.colidx = 10;
+        res.colidx = 6;
         res.markidx = 7;
         res.lsidx = 3;
         res.label = "IPOPT relaxed";
     elseif solname == "SolveIPOPTRegEq"
-        res.colidx = 11;
+        res.colidx = 7;
         res.markidx = 8;
-        res.lsidx = 3;
+        res.lsidx = 4;
         res.label = "IPOPT smoothed";
     else
         error("SOLVER STYLE FOR " + solname + " NOT YET SETUP!");
