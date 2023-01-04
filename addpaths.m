c6c0b6f16259793caabce78be956117cc89c4bef
hallfjonas <hall.f.jonas@gmail.com>
2023-01-04 17:12:15 -0500

    add local paths

---
+++ addpaths.m
@@ -0,0 +1,21 @@
+
+%% These paths may need adjustments
+% Add path to the main solver
+%addpath("/opt/LCQPow");
+
+% Add path to casadi (problems are built with this)
+%addpath("/opt/casadi");
+
+% Add path to gurobi (some variants are solved with this
+%addpath("/opt/gurobi911");
+
+%% Local paths (no changes needed)
+addpath("helpers/");
+addpath("solvers/");
+addpath("MacMPEC/");
+addpath("MacMPEC/helpers/");
+addpath("MacMPEC/MacMPECMatlab/");
+addpath("IVOCP/");
+addpath("IVOCP/helpers");
+addpath("MovingMasses/");
+addpath("MovingMasses/helpers");
